4effc52de54819b60947b48d0367d0ef87bfeeca
laittd <115093798+laittd@users.noreply.github.com>
2023-03-28 19:58:05 +0300

    Изменен текст

--- lab1 консоль/src/main/java/ru/bstu/itz/cherepchenko/lab1/ConsoleMain.java
+++ lab1 консоль/src/main/java/ru/bstu/itz/cherepchenko/lab1/ConsoleMain.java
@@ -13,7 +13,7 @@ public class ConsoleMain {
         a = getFloat();
         System.out.print("Введите 2 число: ");
         b = getFloat();
-        System.out.print("Введите 3 число: ");
+        System.out.print("Введите 10 число: ");
         c = getFloat();
 
         return new float[]{a, b, c};
